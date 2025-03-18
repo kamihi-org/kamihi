854819bd06bfeba1823c21ea1ccc2deb6af9ef26
kiinami <kinami@posteo.net>
2025-03-18 11:22:18 +0100

    build: added test function for documentation generation

--- src/kamihi/__init__.py
+++ src/kamihi/__init__.py
@@ -5,3 +5,17 @@ Kamihi is a Python package for creating and managing Telegram bots.
 """
 
 __version__ = "0.1.0"
+
+
+def hello(name: str) -> str:
+    """
+    Say hello to the user.
+
+    Args:
+        name (str): The name of the user.
+
+    Returns:
+        str: The greeting message.
+
+    """
+    return f"Hello from Kamihi, {name}!"
