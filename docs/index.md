e93da040b12c439e06de249709a9338a08a3ac5c
kiinami <73229276+kiinami@users.noreply.github.com>
2025-03-18 13:07:46 +0100

    docs: initial documentation configuration (#2)
    * build: added dependencies for documentation
    
    * build: added test function for documentation generation
    
    * build: added example section to test function
    
    * build: modified example docstring to use type hints
    
    * docs: basic structure and settings
    
    * ci: added docs action

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# The Kamihi Books
+
+Documentation for the Kamihi framework.
+
+Kamihi is a Python framework for creating and managing Telegram bots.
--- src/kamihi/__init__.py
+++ src/kamihi/__init__.py
@@ -1,7 +1,36 @@
 """
-Kamihi.
+Kamihi is a Python framework for creating Telegram bots.
 
-Kamihi is a Python package for creating and managing Telegram bots.
+This is a long description. It should be multiple lines and give an overview
+of the package functionality. It should also mention the key features and
+advantages of the package.
+
+Attributes:
+    __version__ (str): The version of the package.
+
+License:
+    MIT License. See LICENSE.md file for details.
+
 """
 
 __version__ = "0.1.0"
+
+
+def hello(name: str) -> str:
+    """
+    Say hello to the user.
+
+    Examples:
+        >>> hello("Alice")
+        "Hello from Kamihi, Alice!"
+        >>> hello("Bob")
+        "Hello from Kamihi, Bob!"
+
+    Args:
+        name: The name of the user.
+
+    Returns:
+        The greeting message.
+
+    """
+    return f"Hello from Kamihi, {name}!"
