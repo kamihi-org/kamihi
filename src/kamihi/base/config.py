f6a9d91d879acf52a87a4a95f4af66fb3d61748f
kiinami <kinami@posteo.net>
2025-03-19 11:54:56 +0100

    feat(base): implemented base config class and methods

--- src/kamihi/base/config.py
+++ src/kamihi/base/config.py
@@ -1,16 +1,51 @@
 """
-TODO: one-line module description.
+Configuration module.
 
-TODO: Additional details about the module, its purpose, and any necessary
-background information. Explain what functions or classes are included.
+This module contains the configuration settings for the Kamihi framework. The configuration settings are loaded from
+environment variables and/or a `.env` file. They must begin with the prefix `KAMIHI_`.
 
 License:
     MIT
 
-Examples:
-    [Examples of how to use the module/classes/functions]
-
 Attributes:
-    [List any relevant module-level attributes with types and descriptions]
+    settings (KamihiSettings): Global instance of settings.
 
 """
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class KamihiSettings(BaseSettings):
+    """
+    Defines the configuration schema for the Kamihi framework.
+
+    Attributes:
+        model_config (SettingsConfigDict): Configuration dictionary for environment settings.
+
+    """
+
+    model_config = SettingsConfigDict(
+        env_prefix="KAMIHI_",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+
+# Global instance of settings
+settings = KamihiSettings()
+
+
+def get_settings() -> KamihiSettings:
+    """
+    Get the current settings.
+
+    This function returns the current settings object, which can be used
+    to access or modify configuration options.
+
+    Returns:
+       KamihiSettings: The current settings object.
+
+    """
+    return settings
