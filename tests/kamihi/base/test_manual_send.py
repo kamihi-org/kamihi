1ead35d7fcefe089e5c5fe83fb0ed5b9d4abd6cc
kiinami <kinami@posteo.net>
2025-03-20 13:58:15 +0100

    refactor(base): deleted old tests

--- tests/kamihi/base/test_manual_send.py
+++ tests/kamihi/base/test_manual_send.py
@@ -5,18 +5,3 @@ License:
     MIT
 
 """
-import os
-
-
-def test_alert_manager_init_empty():
-    """Test that AlertManager initializes correctly with no URLs."""
-    from kamihi.base.manual_send import get_notifier
-    notifier = get_notifier()
-    assert len(notifier) == 0
-
-def test_alert_manager_init_urls():
-    """Test that AlertManager initializes correctly with URLs."""
-    os.environ["KAMIHI_ALERT_URLS"] = '["discord://webhook_id/webhook_token", "mailto://domain.com?user=userid&pass=password"]'
-    from kamihi.base.manual_send import get_notifier
-    notifier = get_notifier()
-    assert len(notifier) == 2
