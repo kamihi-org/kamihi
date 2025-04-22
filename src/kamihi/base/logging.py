8c0b2c1cbc17e53af2f1da9d098d5af11b869d82
kiinami <kinami@posteo.net>
2025-04-22 10:34:19 +0200

    refactor(base): log message texts with no color

--- src/kamihi/base/logging.py
+++ src/kamihi/base/logging.py
@@ -47,7 +47,7 @@ def configure_logging(logger: loguru.Logger, settings: LogSettings) -> None:
             format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-            "<level>{message}</level>",
+            "{message}",
             serialize=settings.stdout_serialize,
             enqueue=True,
         )
@@ -59,7 +59,7 @@ def configure_logging(logger: loguru.Logger, settings: LogSettings) -> None:
             format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-            "<level>{message}</level>",
+            "{message}",
             serialize=settings.stderr_serialize,
             enqueue=True,
         )
@@ -71,7 +71,7 @@ def configure_logging(logger: loguru.Logger, settings: LogSettings) -> None:
             format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-            "<level>{message}</level>",
+            "{message}",
             serialize=settings.file_serialize,
             rotation=settings.file_rotation,
             retention=settings.file_retention,
@@ -86,7 +86,7 @@ def configure_logging(logger: loguru.Logger, settings: LogSettings) -> None:
             format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-            "<level>{message}</level>",
+            "{message}",
             filter={"apprise": False},
             enqueue=True,
         )
