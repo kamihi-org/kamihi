d9ed10d9a4ca86c0d674427b14b04a6fb53dc8bb
kiinami <kinami@posteo.net>
2025-04-29 12:11:04 +0200

    refactor(bot): enhance logging and validation in action handling

--- src/kamihi/bot/action.py
+++ src/kamihi/bot/action.py
@@ -6,9 +6,12 @@ License:
 
 """
 
+from __future__ import annotations
+
 import inspect
 from collections.abc import Callable
 
+import loguru
 from loguru import logger
 from telegram import Update
 from telegram.constants import BotCommandLimit
@@ -38,6 +41,7 @@ class Action:
     func: Callable
 
     _valid: bool = True
+    _logger: loguru.Logger
 
     def __init__(self, name: str, commands: list[str], description: str, func: Callable) -> None:
         """
@@ -55,13 +59,15 @@ class Action:
         self.description = description
         self.func = func
 
+        self._logger = logger.bind(action=self.name)
+
         self._validate_commands()
         self._validate_function()
 
         if self.is_valid():
-            logger.debug(f"Successfully registered {self}")
+            self._logger.debug("Successfully registered")
         else:
-            logger.warning(f"Failed to register {self}")
+            self._logger.warning("Failed to register")
 
     def _validate_commands(self) -> None:
         """Filter valid commands and log invalid ones."""
@@ -73,26 +79,46 @@ class Action:
         # Filter out invalid commands
         for cmd in self.commands.copy():
             if not COMMAND_REGEX.match(cmd):
-                logger.warning(
-                    f"Command '/{cmd}' for action '{self.name}' was discarded: "
-                    f"must be {min_len}-{max_len} chars of lowercase letters, digits and underscores"
+                self._logger.warning(
+                    "Command '/{cmd}' was discarded: "
+                    "must be {min_len}-{max_len} chars of lowercase letters, digits and underscores",
+                    cmd=cmd,
+                    min_len=min_len,
+                    max_len=max_len,
                 )
                 self.commands.remove(cmd)
 
         # Mark as invalid if no commands are left
         if not self.commands:
-            logger.warning(f"Action '{self.name}' has no valid commands")
+            self._logger.warning("No valid commands were given")
             self._valid = False
 
     def _validate_function(self) -> None:
         """Validate the function passed."""
         # Check if the function is a coroutine
         if not inspect.iscoroutinefunction(self.func):
-            logger.warning(
-                f"Action '{self.name}' should be a coroutine, define it with 'async def {self.func.__name__}()' "
-                f"instead of 'def {self.func.__name__}()'."
+            self._logger.warning(
+                "Function should be a coroutine, define it with 'async def {name}()' instead of 'def {name}()'.",
+                name=self.func.__name__,
             )
             self._valid = False
+
+        # Check if the function has valid parameters
+        parameters = inspect.signature(self.func).parameters
+        for name, param in parameters.items():
+            if name not in ("update", "context", "logger"):
+                self._logger.warning(
+                    "Invalid parameter '{name}' in function",
+                    name=name,
+                )
+                self._valid = False
+
+            if param.kind == inspect.Parameter.VAR_POSITIONAL or param.kind == inspect.Parameter.VAR_KEYWORD:
+                self._logger.warning(
+                    "Special arguments '*args' and '**kwargs' are not supported in action"
+                    " parameters, they will be ignored. Beware that this may cause issues."
+                )
+                self._valid = False
 
     @property
     def handler(self) -> CommandHandler:
@@ -105,6 +131,11 @@ class Action:
 
     async def __call__(self, update: Update, context: CallbackContext) -> None:
         """Execute the action."""
+        if not self.is_valid():
+            self._logger.warning("Not valid, skipping execution")
+            return
+
+        self._logger.debug("Executing")
         parameters = inspect.signature(self.func).parameters
         pos_args = []
         keyword_args = {}
@@ -116,24 +147,20 @@ class Action:
                     value = update
                 case "context":
                     value = context
-                case _:
-                    logger.warning(f"Unknown parameter '{name}', it will be set to None")
+                case "logger":
+                    value = self._logger
 
             match param.kind:
                 case inspect.Parameter.POSITIONAL_OR_KEYWORD | inspect.Parameter.KEYWORD_ONLY:
                     keyword_args[name] = value
                 case inspect.Parameter.POSITIONAL_ONLY:
                     pos_args.append(value)
-                case inspect.Parameter.VAR_POSITIONAL | inspect.Parameter.VAR_KEYWORD:
-                    logger.warning(
-                        f"Special arguments '*args' and '**kwargs' are not supported in action"
-                        f" parameters, they will be ignored. Beware that this may cause issues."
-                    )
 
-        result = await self.func()
+        result = await self.func(*pos_args, **keyword_args)
         if result is not None:
             await reply_text(update, context, result)
 
+        self._logger.debug("Executed successfully")
         raise ApplicationHandlerStop
 
     def __repr__(self) -> str:
