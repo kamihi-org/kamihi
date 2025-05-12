73f028e9495a546fecc62ee6d2083d64a96245eb
kiinami <kinami@posteo.net>
2025-05-12 10:57:06 +0200

    feat(users): added permission handling in actions

---
+++ src/kamihi/tg/handlers/auth_handler.py
@@ -0,0 +1,55 @@
+"""
+TODO: one-line module description.
+
+TODO: Additional details about the module, its purpose, and any necessary
+background information. Explain what functions or classes are included.
+
+License:
+    MIT
+
+Examples:
+    [Examples of how to use the module/classes/functions]
+
+Attributes:
+    [List any relevant module-level attributes with types and descriptions]
+
+"""
+
+from loguru import logger
+from telegram import Update
+from telegram.ext import BaseHandler
+
+from kamihi.users.users import get_user_from_telegram_id, is_user_authorized
+
+
+class AuthHandler(BaseHandler):
+    """
+    Custom wrapper handler that checks if the user is authorized to use the wrapped handler before executing it.
+
+    Attributes:
+        handler (BaseHandler): the handler to be wrapped.
+        name (str): The name of the action.
+
+    """
+
+    handler: BaseHandler
+    name: str
+
+    def __init__(self, handler: BaseHandler, name: str) -> None:
+        """Initialize the AuthHandler with the callback function."""
+        self.handler = handler
+        self.name = name
+        super().__init__(self.handler.callback)
+
+    def check_update(self, update: Update) -> bool:
+        """Determine if an update should be handled by this handler instance."""
+        if not isinstance(update, Update):
+            return False
+
+        if update.message and update.effective_user:
+            user = get_user_from_telegram_id(update.effective_user.id)
+
+            if user and (user.is_admin or is_user_authorized(user, self.name)):
+                return True
+
+        return False
--- src/kamihi/users/users.py
+++ src/kamihi/users/users.py
@@ -15,7 +15,12 @@ Attributes:
 
 """
 
-from .models import User
+from mongoengine import Q
+
+from kamihi.bot.models import RegisteredAction
+
+from .models import Role, User
+from .models.permission import Permission
 
 
 def get_users() -> list[User]:
@@ -41,3 +46,22 @@ def get_user_from_telegram_id(telegram_id: int) -> User | None:
 
     """
     return User.objects(telegram_id=telegram_id).first()
+
+
+def is_user_authorized(user: User, action: str) -> bool:
+    """
+    Check if a user is authorized tu use a specific action.
+
+    Args:
+        user (User): The user object to check.
+        action (str): The action to check authorization for.
+
+    Returns:
+        bool: True if the user is authorized, False otherwise.
+
+    """
+    action = RegisteredAction.objects(name=action).first()
+    role = Role.objects(users=user).first()
+    permissions = Permission.objects(Q(action=action) & (Q(users=user) | Q(roles=role))).first()
+
+    return bool(permissions)
