b90d90d3bf400fc042e7061c559fdb7912064af6
kiinami <kinami@posteo.net>
2025-05-12 13:50:42 +0200

    refactor(users): enhance user authorization checks with logging

--- src/kamihi/tg/handlers/auth_handler.py
+++ src/kamihi/tg/handlers/auth_handler.py
@@ -49,7 +49,16 @@ class AuthHandler(BaseHandler):
         if update.message and update.effective_user:
             user = get_user_from_telegram_id(update.effective_user.id)
 
-            if user and is_user_authorized(user, self.name):
-                return True
+            if user is None:
+                logger.bind(user_id=update.effective_user.id, action=self.name).debug(
+                    "User not found in the database tried to use action."
+                )
+                return False
 
-        return False
+            if not is_user_authorized(user, self.name):
+                logger.bind(user_id=user.telegram_id, action=self.name).debug(
+                    "User is not authorized to use this action."
+                )
+                return False
+
+        return True
