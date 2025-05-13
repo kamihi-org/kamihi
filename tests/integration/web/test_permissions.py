957ccf12b9fc5f74a90875a5fa4b4e7a778e7657
kiinami <kinami@posteo.net>
2025-05-13 12:38:40 +0200

    test: added start code for playwright testing

---
+++ tests/integration/web/test_permissions.py
@@ -0,0 +1,35 @@
+"""
+TODO: one-line module description.
+
+TODO: Additional details about the module, its purpose, and any necessary
+background information. Explain what functions or classes are included.
+
+License:
+    MIT
+
+Examples:
+    [Examples of how to use the module/classes/functions]
+
+Attributes:
+    [List any relevant module-level attributes with types and descriptions]
+"""
+
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
