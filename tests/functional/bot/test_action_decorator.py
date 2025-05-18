1d9c15a09dd351b1870655331b64f8c515837b54
kiinami <kinami@posteo.net>
2025-05-18 12:10:06 +0200

    test: added functional test case for multiple actions definition

--- tests/functional/bot/test_action_decorator.py
+++ tests/functional/bot/test_action_decorator.py
@@ -41,3 +41,42 @@ class TestActionDecoratorNoParentheses:
         response = await chat.get_response()
 
         assert response.text == "test"
+
+
+class TestActionMultipleDefined:
+    """Test the action decorator with multiple defined actions."""
+
+    @pytest.fixture
+    def user_code(self):
+        """Fixture to provide the user code for the bot."""
+        return {
+            "main.py": dedent("""\
+                              from kamihi import bot
+                             
+                              @bot.action
+                              async def start():
+                                  return "Hello! I'm your friendly bot. How can I help you today?"
+                             
+                              @bot.action
+                              async def start2():
+                                  return "Hello! I'm not your friendly bot."
+                             
+                              bot.start()
+                              """).encode()
+        }
+
+    @pytest.mark.asyncio
+    async def test_action_multiple_defined(self, kamihi, user_in_db, add_permission_for_user, chat: Conversation):
+        """Test the action decorator with multiple defined actions."""
+        add_permission_for_user(user_in_db, "start")
+        add_permission_for_user(user_in_db, "start2")
+
+        await chat.send_message("/start")
+        response = await chat.get_response()
+
+        assert response.text == "Hello! I'm your friendly bot. How can I help you today?"
+
+        await chat.send_message("/start2")
+        response = await chat.get_response()
+
+        assert response.text == "Hello! I'm not your friendly bot."
