e55a523ff9202c5d90c72345df5947665ee6500f
kiinami <kinami@posteo.net>
2025-05-20 18:00:11 +0200

    feat(cli): init command

--- src/kamihi/cli/cli.py
+++ src/kamihi/cli/cli.py
@@ -14,23 +14,36 @@ from .commands import action_app, init_app, run_app, user_app, version_app
 
 app = typer.Typer()
 app.add_typer(version_app)
+app.add_typer(init_app)
 app.add_typer(action_app, name="action")
-app.add_typer(init_app, name="init")
 app.add_typer(run_app, name="run")
 app.add_typer(user_app, name="user")
 
 
-data = {}
+class Context:
+    """
+    Context for the Kamihi CLI utility.
+
+    This class holds the context data for the CLI commands.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the context with default values."""
+        self.cwd: Path = Path.cwd()
+        self.templates: Path = Path(__file__).parent / "templates"
+        self.project: Path = self.cwd
 
 
 @app.callback()
-def main() -> None:
+def main(
+    ctx: typer.Context,
+) -> None:
     """
     Kamihi CLI utility.
 
     This utility provides commands to manage and interact with the Kamihi framework.
     """
-    data["cwd"] = Path.cwd()
+    ctx.obj = Context()
 
 
 if __name__ == "__main__":
