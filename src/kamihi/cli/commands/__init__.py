8f5cc20d8fe13a4d95a86738c00e1705fd94ce01
kiinami <kinami@posteo.net>
2025-05-20 16:22:11 +0200

    refactor(cli): moved version command to its own file

--- src/kamihi/cli/commands/__init__.py
+++ src/kamihi/cli/commands/__init__.py
@@ -10,5 +10,6 @@ from .action import app as action_app
 from .init import app as init_app
 from .run import app as run_app
 from .user import app as user_app
+from .version import app as version_app
 
-__all__ = ["action_app", "init_app", "run_app", "user_app"]
+__all__ = ["version_app", "action_app", "init_app", "run_app", "user_app"]
