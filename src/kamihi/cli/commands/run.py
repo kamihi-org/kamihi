d49686166390727e79f1e97c12aa4a52be8cbba9
kiinami <kinami@posteo.net>
2025-05-20 14:20:43 +0200

    docs(cli): cleaned docstring

--- src/kamihi/cli/commands/run.py
+++ src/kamihi/cli/commands/run.py
@@ -1,17 +1,8 @@
 """
 Kamihi framework project execution.
 
-TODO: Additional details about the module, its purpose, and any necessary
-background information. Explain what functions or classes are included.
-
 License:
     MIT
-
-Examples:
-    [Examples of how to use the module/classes/functions]
-
-Attributes:
-    [List any relevant module-level attributes with types and descriptions]
 
 """
 
