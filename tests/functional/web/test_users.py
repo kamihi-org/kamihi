9daec456be07f83e35f249b121c964f0e7c9e58b
kiinami <kinami@posteo.net>
2025-05-23 10:38:06 +0200

    test: adapted existing functional tests to new setup

--- tests/functional/web/test_users.py
+++ tests/functional/web/test_users.py
@@ -10,13 +10,16 @@ import pytest
 from playwright.async_api import expect
 
 
-@pytest.mark.asyncio
-async def test_create_user(admin_page, test_settings):
-    """Test the creation of a user through the web interface."""
-    await admin_page.get_by_role("link", name=" Users").click()
-    await admin_page.get_by_role("link", name="+ New User").click()
-    await admin_page.get_by_role("spinbutton", name="Telegram id*").click()
-    await admin_page.get_by_role("spinbutton", name="Telegram id*").fill(str(test_settings.user_id))
-    await admin_page.get_by_role("button", name="Save", exact=True).click()
-    await expect(admin_page.locator("#dt_info")).to_contain_text("Showing 1 to 1 of 1 entries")
-    await expect(admin_page.locator("tbody")).to_contain_text(str(test_settings.user_id))
+class TestWebUsersCreate:
+    """Test user creation through the web interface."""
+
+    @pytest.mark.asyncio
+    async def test_create_user(self, admin_page, test_settings):
+        """Test the creation of a user through the web interface."""
+        await admin_page.get_by_role("link", name=" Users").click()
+        await admin_page.get_by_role("link", name="+ New User").click()
+        await admin_page.get_by_role("spinbutton", name="Telegram id*").click()
+        await admin_page.get_by_role("spinbutton", name="Telegram id*").fill(str(test_settings.user_id))
+        await admin_page.get_by_role("button", name="Save", exact=True).click()
+        await expect(admin_page.locator("#dt_info")).to_contain_text("Showing 1 to 1 of 1 entries")
+        await expect(admin_page.locator("tbody")).to_contain_text(str(test_settings.user_id))
