86c2414a30f6e5d7ba36dbdd6f998c4273182663
kiinami <kinami@posteo.net>
2025-05-26 19:05:57 +0200

    test(cli): checking for extra dict in run command with host

--- tests/functional/cli/test_run.py
+++ tests/functional/cli/test_run.py
@@ -59,9 +59,7 @@ def test_run_log_level_invalid(kamihi: KamihiContainer, level: str):
 def test_run_web_host(kamihi: KamihiContainer, host):
     """Test the run command with various valid web host options."""
     kamihi.run_and_wait_for_log(
-        f"kamihi run --host={host}",
-        f"Web server started on http://{host}:4242",
-        "INFO",
+        f"kamihi run --host={host}", f"Web server started on", "INFO", {"host": host, "port": 4242}
     )
 
 
@@ -85,8 +83,9 @@ def test_run_web_port(kamihi: KamihiContainer, port):
     """Test the run command with various valid web port options."""
     kamihi.run_and_wait_for_log(
         f"kamihi run --port={port}",
-        f"Web server started on http://0.0.0.0:{port}",
+        f"Web server started on",
         "INFO",
+        {"host": "0.0.0.0", "port": port},
     )
 
 
