1f321b54a842c9cf74b9eded7a9df4f206d77314
kiinami <kinami@posteo.net>
2025-06-04 10:31:55 +0200

    feat(tg): support sending files as action results
    Closes #77

--- src/kamihi/tg/send.py
+++ src/kamihi/tg/send.py
@@ -6,6 +6,8 @@ License:
 
 """
 
+from pathlib import Path
+
 from loguru import logger
 from telegram import Bot, Message, Update
 from telegram.error import TelegramError
@@ -44,20 +46,83 @@ async def send_text(
         return reply
 
 
-async def reply_text(update: Update, context: CallbackContext, text: str) -> None:
+async def send_file(bot: Bot, chat_id: int, file: Path, reply_to_message_id: int = None) -> Message | None:
+    """
+    Send a file to a chat.
+
+    This function sends a file to a specified chat using the provided bot instance.
+
+    Args:
+        bot (Bot): The Telegram Bot instance.
+        chat_id (int): The ID of the chat to send the file to.
+        file (Path): The file to send.
+        reply_to_message_id (int, optional): The ID of the message to reply to. Defaults to None.
+
+    Returns:
+        Message | None: The response from the Telegram API, or None if an error occurs.
+
+    """
+    lg = logger.bind(chat_id=chat_id, received_id=reply_to_message_id, path=file)
+
+    with lg.catch(exception=TelegramError, message="Failed to send file"):
+        reply = await bot.send_document(
+            chat_id=chat_id,
+            document=file,
+            filename=file.name,
+            reply_to_message_id=reply_to_message_id,
+        )
+        lg.bind(response_id=reply.message_id).debug("File sent")
+        return reply
+
+
+async def send(bot: Bot, chat_id: int, content: str | Path, reply_to_message_id: int = None) -> Message | None:
+    """
+    Send a message to a chat.
+
+    This function sends a message to a specified chat using the provided bot instance.
+    It can handle text and files.
+
+    Args:
+        bot (Bot): The Telegram Bot instance.
+        chat_id (int): The ID of the chat to send the message to.
+        content (str | Path): The content to send, which can be text or a file.
+        reply_to_message_id (int, optional): The ID of the message to reply to. Defaults to None.
+
+    Returns:
+        Message | None: The response from the Telegram API, or None if an error occurs.
+
+    """
+    match content:
+        case str():
+            return await send_text(bot, chat_id, content, reply_to_message_id)
+        case Path():
+            return await send_file(bot, chat_id, content, reply_to_message_id)
+        case _:
+            logger.error("Unsupported content type: {}", type(content))
+            return None
+
+
+async def reply(
+    update: Update,
+    context: CallbackContext,
+    content: str | Path,
+) -> Message | None:
     """
     Reply to a message update.
 
-    Convenience method to send a text message in response to an update.
+    This function replies to a message update with either text or a file.
 
     Args:
         update: the update object
         context: the context object
-        text: the text to send
+        content: the content to send, which can be text or a file
+
+    Returns:
+        Message | None: The response from the Telegram API, or None if an error occurs.
 
     """
     bot = context.bot
     chat_id = update.effective_message.chat_id
     message_id = update.effective_message.message_id
 
-    await send_text(bot, chat_id, text, message_id)
+    return await send(bot, chat_id, content, reply_to_message_id=message_id)
