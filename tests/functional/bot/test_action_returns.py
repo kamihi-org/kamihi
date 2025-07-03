c5f05daab337a1c4405c04e051183a90639883d1
kiinami <kinami@posteo.net>
2025-07-03 11:46:27 +0200

    test(bot): added functional tests for video sending

--- tests/functional/bot/test_action_returns.py
+++ tests/functional/bot/test_action_returns.py
@@ -9,7 +9,7 @@ import pytest
 from telethon import TelegramClient
 from telethon.tl.custom import Conversation, Message
 
-from tests.conftest import random_image
+from tests.conftest import random_image, random_video_path
 
 
 @pytest.mark.asyncio
@@ -368,3 +368,133 @@ async def test_action_returns_list(user_in_db, add_permission_for_user, chat: Co
     for message in messages:
         response: Message = await chat.get_response()
         assert response.text == message
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder",
+    [
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start() -> Annotated[Path, bot.Video()]:
+                    return Path("actions/start/video.mp4")
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start():
+                    return bot.Video(Path("actions/start/video.mp4"))
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start() -> bot.Video:
+                    return bot.Video(Path("actions/start/video.mp4"))
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+    ],
+    ids=["annotated", "class", "class_ra"],
+)
+async def test_action_returns_video(
+    user_in_db, add_permission_for_user, chat: Conversation, tg_client: TelegramClient, actions_folder, tmp_path
+):
+    """Test that the action sends a video to Telegram when a Path is returned and the bot.Video annotation is used."""
+    add_permission_for_user(user_in_db, "start")
+
+    await chat.send_message("/start")
+    response: Message = await chat.get_response()
+
+    assert response.video is not None
+
+    path = tmp_path / "video.mp4"
+    await tg_client.download_media(response, str(path))
+    assert path.exists()
+    assert path.stat().st_size > 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder",
+    [
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start() -> Annotated[Path, bot.Video(caption="This is a video caption.")]:
+                    return Path("actions/start/video.mp4")
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start():
+                    return bot.Video(Path("actions/start/video.mp4"), caption="This is a video caption.")
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from kamihi import bot
+                from pathlib import Path
+                from typing import Annotated
+                             
+                @bot.action
+                async def start() -> bot.Video:
+                    return bot.Video(Path("actions/start/video.mp4"), caption="This is a video caption.")
+            """,
+            "start/video.mp4": random_video_path().read_bytes(),
+        },
+    ],
+    ids=["annotated", "class", "class_ra"],
+)
+async def test_action_returns_video_captioned(
+    user_in_db, add_permission_for_user, chat: Conversation, tg_client: TelegramClient, actions_folder, tmp_path
+):
+    """Test that the action sends a video with a caption to Telegram when a Path is returned and the bot.Video annotation is used."""
+    add_permission_for_user(user_in_db, "start")
+
+    await chat.send_message("/start")
+    response: Message = await chat.get_response()
+
+    assert response.text == "This is a video caption."
+
+    assert response.video is not None
+
+    path = tmp_path / "video.mp4"
+    await tg_client.download_media(response, str(path))
+    assert path.exists()
+    assert path.stat().st_size > 0
