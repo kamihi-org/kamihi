647de542c52c23fe468ac763a2238813e725ceb4
kiinami <kinami@posteo.net>
2025-07-06 13:41:00 +0200

    test(tg): functional tests for media group sending

--- tests/functional/bot/test_action_returns.py
+++ tests/functional/bot/test_action_returns.py
@@ -490,3 +490,105 @@ async def test_action_returns_list(user_in_db, add_permission_for_user, chat: Co
     for message in messages:
         response: Message = await chat.get_response()
         assert response.text == message
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder,number_of_messages",
+    [
+        (
+            {
+                "start/__init__.py": "",
+                "start/start.py": """\
+                    from pathlib import Path
+                    from kamihi import bot
+                
+                    @bot.action
+                    async def start() -> list[bot.Photo]:
+                        return [
+                            bot.Photo(Path("actions/start/image.jpg")),
+                            bot.Photo(Path("actions/start/image.jpg")),
+                            bot.Photo(Path("actions/start/image.jpg")),
+                        ]
+                """,
+                "start/image.jpg": random_image(),
+            },
+            3,
+        ),
+        (
+            {
+                "start/__init__.py": "",
+                "start/start.py": """\
+                    from pathlib import Path
+                    from kamihi import bot
+                
+                    @bot.action
+                    async def start() -> list[bot.Photo | bot.Video]:
+                        return [
+                            bot.Photo(Path("actions/start/image.jpg")),
+                            bot.Video(Path("actions/start/video.mp4")),
+                            bot.Photo(Path("actions/start/image.jpg")),
+                            bot.Video(Path("actions/start/video.mp4")),
+                        ]
+                """,
+                "start/image.jpg": random_image(),
+                "start/video.mp4": random_video_path().read_bytes(),
+            },
+            4,
+        ),
+        (
+            {
+                "start/__init__.py": "",
+                "start/start.py": """\
+                    from pathlib import Path
+                    from kamihi import bot
+                
+                    @bot.action
+                    async def start() -> list[bot.Audio]:
+                        return [
+                            bot.Audio(Path("actions/start/audio.mp3")),
+                            bot.Audio(Path("actions/start/audio.mp3")),
+                            bot.Audio(Path("actions/start/audio.mp3")),
+                        ]
+                """,
+                "start/audio.mp3": random_audio(),
+            },
+            3,
+        ),
+        (
+            {
+                "start/__init__.py": "",
+                "start/start.py": """\
+                    from pathlib import Path
+                    from kamihi import bot
+                
+                    @bot.action
+                    async def start() -> list[bot.Document]:
+                        return [
+                            bot.Document(Path("actions/start/file.txt")),
+                            bot.Document(Path("actions/start/file.txt")),
+                            bot.Document(Path("actions/start/file.txt")),
+                        ]
+                """,
+                "start/file.txt": "This is a file.",
+            },
+            3,
+        ),
+    ],
+)
+async def test_action_returns_group_media(
+    user_in_db, add_permission_for_user, chat: Conversation, actions_folder, number_of_messages
+):
+    """Test actions that return multiple messages."""
+    add_permission_for_user(user_in_db, "start")
+
+    await chat.send_message("/start")
+
+    ids = []
+    for _ in range(number_of_messages):
+        response: Message = await chat.get_response()
+        assert response.media is not None
+        ids.append(response.grouped_id)
+
+    assert all(i == ids[0] for i in ids)
