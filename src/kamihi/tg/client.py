3b55704c7491b44366ff47ba9823d948744d636e
kiinami <kinami@posteo.net>
2025-07-24 14:15:55 +0200

    test(tg): proper TelegramClient unit tests

--- src/kamihi/tg/client.py
+++ src/kamihi/tg/client.py
@@ -24,7 +24,6 @@ from telegram.ext import (
     Application,
     ApplicationBuilder,
     BaseHandler,
-    CallbackContext,
     Defaults,
     DictPersistence,
     MessageHandler,
@@ -86,13 +85,14 @@ class TelegramClient:
 
         # Register the handlers
         for handler in handlers:
-            with logger.catch(exception=TelegramError, message="Failed to register handler"):
+            with logger.catch(exception=TelegramError, level="ERROR", message="Failed to register handler"):
                 self._app.add_handler(handler)
 
         # Register the default handlers
-        if settings.responses.default_enabled:
-            self._app.add_handler(MessageHandler(filters.TEXT, default), group=1000)
-        self._app.add_error_handler(error)
+        with logger.catch(exception=TelegramError, level="ERROR", message="Failed to register default handlers"):
+            if settings.responses.default_enabled:
+                self._app.add_handler(MessageHandler(filters.TEXT, default), group=1000)
+            self._app.add_error_handler(error)
 
     async def reset_scopes(self) -> None:  # noqa: ARG002
         """
@@ -105,7 +105,7 @@ class TelegramClient:
             return
 
         with logger.catch(exception=TelegramError, message="Failed to reset scopes"):
-            await self._app.bot.set_my_commands(commands=[])
+            await self._app.bot.delete_my_commands()
             logger.debug("Scopes erased")
 
     async def set_scopes(self, scopes: dict[int, list[BotCommand]]) -> None:
@@ -124,24 +124,13 @@ class TelegramClient:
             lg = logger.bind(user_id=user_id, commands=[command.command for command in commands])
             with lg.catch(
                 exception=TelegramError,
-                message="Failed to set scopes for user {user_id}",
+                message="Failed to set scopes",
             ):
                 await self._app.bot.set_my_commands(
                     commands=commands,
                     scope=BotCommandScopeChat(user_id),
                 )
                 lg.debug("Scopes set")
-
-    def register_run_once_job(self, callback: callable, when: int) -> None:
-        """
-        Add a job to run once.
-
-        Args:
-            callback (callable): The callback function to run.
-            when (int): second from now to run the job.
-
-        """
-        self._app.job_queue.run_once(callback, when)
 
     def run(self) -> None:
         """Run the Telegram bot."""
