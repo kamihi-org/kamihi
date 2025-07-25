da6de4dba070490a208eab2307d2a6917a543609
kiinami <kinami@posteo.net>
2025-07-25 11:44:37 +0200

    test(datasources): module docs

--- tests/unit/datasources/__init__.py
+++ tests/unit/datasources/__init__.py
@@ -1,16 +1,6 @@
 """
-TODO: one-line module description.
-
-TODO: Additional details about the module, its purpose, and any necessary 
-background information. Explain what functions or classes are included.
+Tests for the datasources module.
 
 License:
     MIT
-
-Examples:
-    [Examples of how to use the module/classes/functions]
-    
-Attributes:
-    [List any relevant module-level attributes with types and descriptions]
 """
-
