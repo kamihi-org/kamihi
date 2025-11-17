bfafa3f031410abf16face85ae5b80c776809769
kiinami <kinami@posteo.net>
2025-11-17 11:21:11 +0100

    test(bot): pagination tests

--- tests/bot/test_action_returns.py
+++ tests/bot/test_action_returns.py
@@ -6,6 +6,7 @@ License:
 """
 
 import random
+from asyncio import sleep
 
 import pytest
 from telethon import TelegramClient
@@ -681,3 +682,159 @@ async def test_action_returns_group_media(
         ids.append(response.grouped_id)
 
     assert all(i == ids[0] for i in ids)
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder",
+    [
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from jinja2 import Template
+                from kamihi import bot
+                             
+                @bot.action
+                async def start(template: Template) -> bot.Pages:
+                    return bot.Pages([i for i in range(50)], template)
+            """,
+            "start/start.md.jinja": """\
+                Them numbers:
+                {% for num in data %}
+                - {{ num }}
+                {% endfor %}
+            """,
+        },
+    ],
+)
+async def test_action_returns_pages(
+    user, add_permission_for_user, chat: Conversation, actions_folder
+):
+    """Test actions that return paginated messages."""
+    add_permission_for_user(user["telegram_id"], "start")
+
+    await chat.send_message("/start")
+    response: Message = await chat.get_response()
+
+    assert "Them numbers:" in response.text
+    for i in range(5):
+        assert f"\u2981 {i}" in response.text
+
+    # Press the Page 2 button
+    await response.click(1)
+
+    # Get the edited message
+    response: Message = await chat.get_edit()
+
+    assert "Them numbers:" in response.text
+    for i in range(5, 10):
+        assert f"\u2981 {i}" in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder",
+    [
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from jinja2 import Template
+                from kamihi import bot
+                             
+                @bot.action
+                async def start(template: Template, template_first_page: Annotated[Template, "first_page.md.jinja"]) -> bot.Pages:
+                    return bot.Pages([i for i in range(50)], template, first_page_template=template_first_page)
+            """,
+            "start/start.md.jinja": """\
+                Them numbers:
+                {% for num in data %}
+                - {{ num }}
+                {% endfor %}
+            """,
+            "start/first-page.md.jinja": """\
+                This is a wonderful list of them numbers. Use the buttons below to navigate.
+            """,
+        },
+    ],
+)
+async def test_action_returns_pages_with_first_page(
+    user, add_permission_for_user, chat: Conversation, actions_folder
+):
+    """Test actions that return paginated messages with a first page."""
+    add_permission_for_user(user["telegram_id"], "start")
+
+    await chat.send_message("/start")
+    response: Message = await chat.get_response()
+
+    assert "This is a wonderful list of them numbers." in response.text
+
+    # Press the Next button to go to the second page (which is the first data page)
+    await response.click(1)
+
+    # Get the edited message
+    response: Message = await chat.get_edit()
+
+    assert "Them numbers:" in response.text
+    for i in range(5):
+        assert f"\u2981 {i}" in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("kamihi")
+@pytest.mark.parametrize(
+    "actions_folder",
+    [
+        {
+            "start/__init__.py": "",
+            "start/start.py": """\
+                from jinja2 import Template
+                from kamihi import bot
+                             
+                @bot.action
+                async def start(template: Template) -> bot.Pages:
+                    return bot.Pages([i for i in range(50)], template)
+            """,
+            "start/start.md.jinja": """\
+                Them numbers:
+                {% for num in data %}
+                - {{ num }}
+                {% endfor %}
+            """,
+        },
+    ],
+)
+@pytest.mark.parametrize(
+    "config_file",
+    [
+        {
+            "kamihi.yaml": """\
+                db:
+                    pages_expiration_days: 0.00002
+            """,
+        },
+    ]
+)
+async def test_action_returns_pages_expired(
+    user, add_permission_for_user, chat: Conversation, actions_folder, config_file
+):
+    """Test actions that return paginated messages that have expired."""
+    add_permission_for_user(user["telegram_id"], "start")
+
+    await chat.send_message("/start")
+    response: Message = await chat.get_response()
+
+    assert "Them numbers:" in response.text
+    for i in range(5):
+        assert f"\u2981 {i}" in response.text
+
+    await sleep(2)
+
+    # Press the Page 2 button
+    await response.click(1)
+
+    # Get the edited message
+    response: Message = await chat.get_edit()
+
+    assert "This paginated message has expired" in response.text
